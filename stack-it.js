5405a337e4f253b3b656039874c010d2b30da081
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2014-12-18 18:00:34 -0700

    Restructure code, hold game variables in an array, and paint to grid

--- stack-it.js
+++ stack-it.js
@@ -1,29 +1,47 @@
-var columns = 9;
-var rows = 12;
-var startingSpeed = 500;
-var endingSpeed = 100;
-
-// Output to the console (I'll be using this a lot)
-function $c(content) {
-	console.log(content);
+var game = {
+	columns: 9,
+	rows: 12,
+	startingSpeed : 260,
+	endingSpeed : 40,
+	selector: 'body',
+	calc: {
+		incrementingSpeed: null,
+		currentSpeed: null,
+		twoCells: null,
+		oneCell: null
+	},
+	active: {
+		row: null,
+		cells: [],
+		lastCell: null,
+		right: true
+	},
+	tower: []
 }
 
-/*
-createTable
-Creates a table in an element
-@param {string} elementId	Supply an element id, but not the element itself
-@param {number} columns		Number of columns the table will have
-@param {number} rows		Number of rows the table will have
-@returns {element} table	Appends a table onto the provided element
-*/
-function createTable(elementId) {
-	var grid = document.getElementById(elementId);
+var createGame = function() {
+	calculateVars();
+	createGrid();
+	handleCellSize();
+	startGame();
+}
+
+var calculateVars = function() {
+	game.calc.incrementingSpeed = Math.floor((game.startingSpeed - game.endingSpeed) / game.rows);
+	game.calc.currentSpeed = game.startingSpeed;
+	game.calc.twoCells = game.rows - (game.rows / 3);
+	game.calc.oneCell = game.rows - (game.rows / 3) * 2;
+}
+
+var createGrid = function() {
+	
+	var grid = document.querySelector(game.selector);
+	
 	var table = document.createElement('table');
-	// I'm going to start these loops at 1 as it will make more sense for rows and columns
-	for (var r = 1; r < rows + 1; r++) {
+	for (var r = 1; r < game.rows + 1; r++) {
 		var tr = document.createElement('tr');
 		tr.id = 'r' + r;
-		for (var c = 1; c < columns + 1; c++) {
+		for (var c = 1; c < game.columns + 1; c++) {
 			var td = document.createElement('td');
 			td.id = 'r' + r + 'c' + c
 			tr.appendChild(td)
@@ -33,19 +51,11 @@ function createTable(elementId) {
 	grid.appendChild(table);
 }
 
-function handleCellSize() {
-	$c('Inner Width: ' + window.innerWidth);
-	$c('Inner Height: ' + window.innerHeight);
-	
-	var maxCellSize = Math.floor(window.innerHeight / rows);
-	$c('Maximum Cell Size: ' + maxCellSize);
-	$c('The table should be: ' + maxCellSize * columns);
-	
-	if (columns * maxCellSize > window.innerWidth) {
-		$c('Basing height off of width instead');
-		maxCellSize = Math.floor(window.innerWidth / columns);
+var handleCellSize = function() {
+	var maxCellSize = Math.floor(window.innerHeight / game.rows);
+	if (game.columns * maxCellSize > window.innerWidth) {
+		maxCellSize = Math.floor(window.innerWidth / game.columns);
 	}
-	
 	var cells = document.getElementsByTagName('td');
 	for (var i = 0; i < cells.length; i++) {
     	cells[i].style.width = maxCellSize + 'px';
@@ -53,7 +63,65 @@ function handleCellSize() {
 	}
 }
 
-window.addEventListener('load', function() {
-	createTable('grid');
-	handleCellSize();
-});
+var startGame = function() {
+	selectRowCells(game.rows);
+	paintGrid();
+	bounce();
+}
+
+var selectRowCells = function(row) {
+	game.active.row = row;
+	game.active.cells = [];
+	var activeCount;
+	if (row <= game.calc.oneCell) {
+		activeCount = 1;
+	}
+	if (row > game.calc.oneCell && row <= game.calc.twoCells) {
+		activeCount = 2;
+	}
+	if (row > game.calc.twoCells) {
+		activeCount = 3;
+	}
+	var startingCell = Math.floor(Math.random() * ((game.columns - activeCount + 1) - 1 + 1)) + 1;
+	for (var i = 0; i < activeCount; i++) {
+		game.active.cells.push(startingCell + i);
+	}
+}
+
+var bounce = function() {
+	if (game.active.cells[game.active.cells.length - 1] == game.columns) {
+		game.active.right = false;
+	}
+	setInterval(function(){
+		if (game.active.right) {
+			var nextCell = game.active.cells[game.active.cells.length - 1] + 1;
+			if (nextCell <= game.columns) {
+				game.active.cells.push(nextCell);
+				game.active.lastCell = game.active.cells.shift();
+			}
+			if (nextCell == game.columns) {
+				game.active.right = false;
+			}
+		}
+		else if (!game.active.right) {
+			var nextCell = game.active.cells[0] - 1;
+			if (nextCell >= 1) {
+				game.active.cells.unshift(nextCell);
+				game.active.lastCell = game.active.cells.pop();
+			}
+			if (nextCell == 1) {
+				game.active.right = true;
+			}
+		}
+		paintGrid();
+	}, game.calc.currentSpeed);
+}
+
+var paintGrid = function() {
+	for (var i = 0; i < game.active.cells.length; i++) {
+		document.getElementById('r' + game.active.row + 'c' + game.active.cells[i]).className = 'activeCell';
+	}
+	if (game.active.lastCell) {
+		document.getElementById('r' + game.active.row + 'c' + game.active.lastCell).className = 'inactiveCell';
+	}
+}
